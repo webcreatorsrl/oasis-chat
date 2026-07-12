1290015924b2cdf78131da5b07787663916e5a81
webcreatorsrl <webcreatorsrl@outlook.com>
2026-07-12 14:33:05 +0200

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "429606193"
+    serviceWorkerVersion: "2031626427"
   }
 });
